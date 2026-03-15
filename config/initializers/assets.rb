6211aef27c9336272a724cb1559d9b3ce0fa1738
Christopher Baptiste <chrisbaptiste83@gmail.com>
2026-03-15 01:20:44 -0700

    fix dependency issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
-
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add the builds directory so stylesheet_link_tag/javascript_include_tag "application" resolve correctly.
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
